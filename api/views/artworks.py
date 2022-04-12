65b3e3b76e1dadf822155b582c6dbd4bbd7b54c4
Anthony Madia <aamadia24@gmail.com>
2022-04-11 20:51:03 -0400

    Finish create controller

--- api/views/artworks.py
+++ api/views/artworks.py
@@ -1,24 +1,54 @@
 from flask import Blueprint, jsonify, request
+import cloudinary.uploader
+from dotenv import load_dotenv
+import os
 from api.middleware import login_required, read_token
-
 from api.models.db import db
 from api.models.artwork import Artwork
 
+load_dotenv()
+
 artworks = Blueprint('artworks', 'artworks')
+
+# cloudinary.config(cloud_name = CLOUD_NAME, api_key=API_KEY, 
+#     api_secret=API_SECRET)
+
+cloudinary.config(cloud_name = os.getenv('CLOUD_NAME'), api_key=os.getenv('API_KEY'), 
+    api_secret=os.getenv('API_SECRET'))
 
 @artworks.route('/', methods=["POST"])
 @login_required
 def create():
-   print('sanity check!!!!!!!!!!!!!!!!!!!!')
-   print('Our request files: ', request.files)
-   print('Our request object size: ', len(request.files))
-   for entries in request.files:
-      print('Entry: ',request.files[entries])
+   # for entries in request.files:
+   #    print('Entry: ',request.files[entries])
+
+   # upload content AND style image to cloudinary
+   uploadResultContent = cloudinary.uploader.upload(request.files['content-image'])
+   uploadResultStyle = cloudinary.uploader.upload(request.files['style-image'])
+   # print(uploadResultStyle)
+
+   contentImageURL = uploadResultContent['url']
+   styleImageURL = uploadResultStyle['url']
+   generatedImageURL = uploadResultContent['url']
+
+   # print('content image url: ', contentImageURL)
+   profile = read_token(request)
+
+   data = {
+      "artworkLink": generatedImageURL,
+      "contentLink": contentImageURL,
+      "styleLink": styleImageURL,
+      "profile_id": profile["id"]
+   }
+
+   artwork = Artwork(**data)
+   db.session.add(artwork)
+   db.session.commit()
    #data = request.get_json()
    #print('our data: ', data)
    # profile = read_token(request)
-   # data["profile_id"] = profile["id"]
+   #data["profile_id"] = profile["id"]
    # cat = Cat(**data)
    # db.session.add(cat)
    # db.session.commit()
-   return jsonify({'testing':'testing'}), 201
+   return jsonify(artwork.serialize()), 201
